3ff283c49c506f0a9901e97f080c8f0087fb4ab9
m4iyama <myam0118@gmail.com>
2018-10-30 19:54:48 +0900

    gitignore firebaseConfig

--- src/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export default {
-  apiKey: 'AIzaSyBXROWpExNPk5of3uZSVrMrcgKIaRhxgIE',
-  authDomain: 'chat-react-redux-firebase.firebaseapp.com',
-  databaseURL: 'https://chat-react-redux-firebase.firebaseio.com',
-  projectId: 'chat-react-redux-firebase',
-  storageBucket: 'chat-react-redux-firebase.appspot.com',
-  messagingSenderId: '1083078158002',
-}
